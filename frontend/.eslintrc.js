1e94070921022b69932ec4e01a0da81754eb4b1b
daisuke721 <d.magome.07@gmail.com>
2025-02-07 20:20:34 +0900

    chore: ESLintの導入と設定 #5

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  // 必要に応じて無視するパターンを指定
+  ignorePatterns: ['node_modules', '.next'],
+  // Next.js + Prettier の推奨設定を使用
+  extends: ['next', 'prettier'],
+  // JS/JSX のみ使うなら parserOptions で ecmaVersion, sourceType を指定
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // 好みでルール追加・上書き
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+    // Next.js では React import が不要なので無効化
+    'react/react-in-jsx-scope': 'off',
+  },
+};
