33fa9eae20164813821844f298b9d12dc7c47769
daisuke721 <d.magome.07@gmail.com>
2025-02-19 15:32:32 +0900

    cange: 拡張子をmjsに変更しファイルも修正 #9

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -1,17 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        noto: 'var(--font-noto-sans-jp), sans-serif',
+        roboto: 'var(--font-roboto), sans-serif',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        sprayGreen: '#A4D5BD',
+        honey: '#E7BB5E',
       },
     },
   },
   plugins: [],
 };
+
+export default config;
