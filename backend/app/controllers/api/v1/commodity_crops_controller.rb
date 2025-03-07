e3b0c1383ed3ec57010a9eebac08922a5ffd4d7c
daisuke721 <d.magome.07@gmail.com>
2025-03-08 02:05:39 +0900

    change: renderを直接json形式での変換を指定していたのでserializerから参照するように変更 #12

--- backend/app/controllers/api/v1/commodity_crops_controller.rb
+++ backend/app/controllers/api/v1/commodity_crops_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::CommodityCropsController < ApplicationController
   def index
     commodity_crops = CommodityCrop.includes(:crop, :commodity_crop_images).all
-    render json: commodity_crops, include: [:crop, :commodity_crop_images]
+    render json: commodity_crops, each_serializer: CommodityCropListSerializer
   end
 
   def show
     commodity_crop = CommodityCrop.includes(:crop, :commodity_crop_images).find(params[:id])
-    render json: commodity_crop, include: [:crop, :commodity_crop_images]
+    render json: commodity_crop, serializer: CommodityCropDetailSerializer
   end
 
   def create
@@ -14,7 +14,7 @@ class Api::V1::CommodityCropsController < ApplicationController
 
     if commodity_crop.save
       # 画像が送信されたら画像があるかpresent?で確認しeach文で1つずつcommodity_crop_imagesに保存
-      if params[:commodity_crop][:images].present?
+      if params.dig(:commodity_crop, :images).present?
         params[:commodity_crop][:images].each do |image|
           # commodity_crop_images.create で CommodityCropImage レコードを作成
           commodity_crop_image = commodity_crop.commodity_crop_images.create
@@ -22,7 +22,7 @@ class Api::V1::CommodityCropsController < ApplicationController
         end
       end
 
-      render json: commodity_crop, include: [commodity_crop_images: { methods: :image_url }], status: :created
+      render json: commodity_crop, serializer: CommodityCropDetailSerializer, status: :created
     else
       render json: { errors: commodity_crop.errors.full_messages }, status: :unprocessable_entity
     end
