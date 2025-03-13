341338f45439953cc425f8308f4eebc138594ad7
daisuke721 <d.magome.07@gmail.com>
2025-03-13 16:44:59 +0900

    change: commodity_crop_imagesを明示的に指定 #14

--- backend/app/serializers/commodity_crop_list_serializer.rb
+++ backend/app/serializers/commodity_crop_list_serializer.rb
@@ -2,6 +2,10 @@ class CommodityCropListSerializer < ActiveModel::Serializer
   # 作物一覧時に必要な基本情報
   attributes :id, :name, :capacity, :price
 
-  # CommodityCropImageSerializerを利用し、作物に関連する画像を取得
-  has_many :commodity_crop_images, serializer: CommodityCropImageSerializer
+  # commodity_crop_images を明示的に追加
+  attribute :commodity_crop_images do
+    object.commodity_crop_images.map do |image|
+      { image_url: Rails.application.routes.url_helpers.rails_blob_url(image.image, host: ENV.fetch('BACKEND_ORIGIN', 'http://localhost:8000')) }
+    end
+  end
 end
