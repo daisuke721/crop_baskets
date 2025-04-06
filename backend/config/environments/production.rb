35a213da673693b936657da3f95f156e6a753c9b
daisuke721 <d.magome.07@gmail.com>
2025-04-06 11:13:17 +0900

    change: default_urlをconfigureのブロック外に記述 #35

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -97,3 +97,6 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
+
+# Railsが自動でURLを作成するベースドメインを指定
+Rails.application.routes.default_url_options[:host] = ENV.fetch("RAILS_API_HOST")
