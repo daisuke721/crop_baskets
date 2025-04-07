ab0844c297d4cd9b47aebbee70c4f555457629d6
daisuke721 <d.magome.07@gmail.com>
2025-04-07 19:49:21 +0900

    add: オリジンの追加 #47

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -6,8 +6,7 @@
 # Read more: https://github.com/cyu/rack-cors
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.env.production? ? 'https://cropbaskets.jp' : 'http://localhost:3000'
-    puts puts "CORS Origin: #{origin}"
+    origins ['https://cropbaskets.jp', 'https://www.cropbaskets.jp', 'http://localhost:3000']
 
     resource "*",
       headers: :any,
