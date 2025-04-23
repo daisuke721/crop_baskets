bf0f994395d34194c9eae851f5ccb01cca35da8d
daisuke721 <d.magome.07@gmail.com>
2025-04-24 00:24:16 +0900

    change: ヘッダーの修正 #66

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,32 +1,15 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
-
-import { BsCart } from 'react-icons/bs';
-import { CiMenuBurger } from 'react-icons/ci';
 
 const Header = () => {
   return (
     <>
-      <header className="h-28 w-full border-b flex justify-between items-center">
-        <div className="flex place-items-center px-5">
-          <h1>
-            <Link href="/">
-              <Image src="/logo.png" alt="Crop Baskets" width={150} height={100} />
-            </Link>
-          </h1>
-        </div>
-        <div className="flex mx-10">
-          <div>
-            <Link href="/cart">
-              <BsCart className="text-3xl text-sprayGreen" />
-            </Link>
-          </div>
-          <div className="px-5 text-xl text-sprayGreen">|</div>
-          <div>
-            <CiMenuBurger className="text-3xl text-sprayGreen" />
-          </div>
-        </div>
+      <header className="h-16 w-full border-b border-b-gray-100">
+        <h1 className="flex justify-center py-2">
+          <Link href="/">
+            <Image src="/crop-baskets-logo.png" alt="Crop Baskets" width={150} height={50} />
+          </Link>
+        </h1>
       </header>
     </>
   );
