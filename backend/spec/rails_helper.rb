6b395e783be7d38fd41b6a985082ec1cf74c11a6
daisuke721 <d.magome.07@gmail.com>
2025-05-30 00:43:02 +0900

    chore: FactoryBotのメソッドを使用する際、モジュール名を省略 #84

--- backend/spec/rails_helper.rb
+++ backend/spec/rails_helper.rb
@@ -69,4 +69,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # FactoryBotのメソッドを使用する際、モジュール名を省略する
+  config.include FactoryBot::Syntax::Methods
 end
