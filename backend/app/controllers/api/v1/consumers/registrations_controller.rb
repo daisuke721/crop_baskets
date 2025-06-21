22c015912096b4b48de37422a964e879bd8a8454
daisuke721 <d.magome.07@gmail.com>
2025-06-21 23:57:13 +0900

    add: 手動でトークンを生成し、ヘッダーに追加する処理を実装 #88

--- backend/app/controllers/api/v1/consumers/registrations_controller.rb
+++ backend/app/controllers/api/v1/consumers/registrations_controller.rb
@@ -3,8 +3,15 @@ class Api::V1::Consumers::RegistrationsController < Devise::RegistrationsControl
 
   private
 
+  def sign_up_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+
   def respond_with(resource, _opts = {})
     if resource.persisted?
+      token = Warden::JWTAuth::UserEncoder.new.call(resource, :consumer, nil).first
+      response.set_header('Authorization', "Bearer #{token}")
+
       render json: { message: '登録成功（Consumer）', consumer: resource }, status: :ok
     else
       render json: { message: '登録失敗（Consumer）', errors: resource.errors.full_messages }, status: :unprocessable_entity
--- backend/app/controllers/api/v1/consumers/sessions_controller.rb
+++ backend/app/controllers/api/v1/consumers/sessions_controller.rb
@@ -4,6 +4,9 @@ class Api::V1::Consumers::SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
+    token = Warden::JWTAuth::UserEncoder.new.call(resource, :consumer, nil).first
+    response.set_header('Authorization', "Bearer #{token}")
+
     render json: { message: 'ログイン成功（Consumer）', consumer: resource }, status: :ok
   end
 
