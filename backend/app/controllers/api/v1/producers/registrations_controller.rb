0937909b0c88e2c7e870df4f826655a7cb7ff77d
daisuke721 <d.magome.07@gmail.com>
2025-06-22 23:49:16 +0900

    add: 手動でトークンを生成し、ヘッダーに追加する処理を実装 #88

--- backend/app/controllers/api/v1/producers/registrations_controller.rb
+++ backend/app/controllers/api/v1/producers/registrations_controller.rb
@@ -3,11 +3,17 @@ class Api::V1::Producers::RegistrationsController < Devise::RegistrationsControl
 
   private
 
+  def sign_up_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+
   def respond_with(resource, _opts = {})
     if resource.persisted?
+      token = Warden::JWTAuth::UserEncoder.new.call(resource, :producer, nil).first
+      response.set_header('Authorization', "Bearer #{token}")
       render json: { message: '登録成功（Producer）', producer: resource }, status: :ok
     else
-      render json: { message: '登録失敗（Producer）', errors: resource.errors.full_message }, status: :unprocessable_entity
+      render json: { message: '登録失敗（Producer）', errors: resource.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
--- backend/app/controllers/api/v1/producers/sessions_controller.rb
+++ backend/app/controllers/api/v1/producers/sessions_controller.rb
@@ -4,6 +4,8 @@ class Api::V1::Producers::SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
+    token = Warden::JWTAuth::UserEncoder.new.call(resource, :producer, nill).first
+    response.set_header('Authorization', "Bearer #{token}")
     render json: { message: 'ログイン成功（Producer）', producer: resource }, status: :ok
   end
 
