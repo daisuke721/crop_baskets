7af94137bf246aae63d049586802a8178791779e
daisuke721 <d.magome.07@gmail.com>
2025-06-29 23:35:22 +0900

    change: producer_idと紐付け #88

--- backend/db/migrate/20250210181931_create_commodity_crops.rb
+++ backend/db/migrate/20250210181931_create_commodity_crops.rb
@@ -2,7 +2,7 @@ class CreateCommodityCrops < ActiveRecord::Migration[7.2]
   def change
     create_table :commodity_crops do |t|
       # Want機能のため、producer_idは一旦、null: true, foreign_key: falseに設定
-      t.references :producer, null: true, foreign_key: false
+      t.references :producer, null: false, foreign_key: true
       t.references :crop, null: false, foreign_key: true
       t.string :name, null: false
       t.string :variety, null: false, limit: 50
