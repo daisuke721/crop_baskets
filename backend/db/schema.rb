9fcf990ca56e57f640abf5bafc3bf7c352b99655
daisuke721 <d.magome.07@gmail.com>
2025-07-17 23:44:30 +0900

    add: マイグレーションの実行 #93

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_12_134208) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_17_143352) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -122,9 +122,21 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_12_134208) do
     t.index ["reset_password_token"], name: "index_producers_on_reset_password_token", unique: true
   end
 
+  create_table "receiving_points", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "producer_id", null: false
+    t.string "name"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["producer_id"], name: "index_receiving_points_on_producer_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "cart_items", "commodity_crops"
   add_foreign_key "commodity_crop_images", "commodity_crops"
   add_foreign_key "commodity_crops", "crops"
+  add_foreign_key "receiving_points", "producers"
 end
