e892fdea52cd7579c2a3df0d23049d3fa2643ba5
daisuke721 <d.magome.07@gmail.com>
2025-07-17 23:43:13 +0900

    add: ReceivingPointモデルにバリデーションを実装 #93

--- backend/app/models/receiving_point.rb
+++ backend/app/models/receiving_point.rb
@@ -1,3 +1,7 @@
 class ReceivingPoint < ApplicationRecord
   belongs_to :producer
+
+  # バリデーション
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :address, presence: true, length: { maximum: 100 }
 end
