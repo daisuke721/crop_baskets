2549221308316e3d7bc1c8b43b2907fb7caa4443
daisuke721 <d.magome.07@gmail.com>
2025-07-23 00:07:06 +0900

    add: receiving_pointsのルーティングを追加 #93

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
       resources :orders, only: [:index, :new, :create, :show]
       # 簡単出品用のルーティング
       resources :simple_listings, only: [:create]
+      # 受け取りポイントのルーティング
+      resources :receiving_points
     end
   end
 
