708d8c4a30c58004fdfb9fd98b562530533afeb3
daisuke721 <d.magome.07@gmail.com>
2025-07-30 00:01:14 +0900

    add: producer_information_serializerの実装 #96

---
+++ backend/app/serializers/producer_information_serializer.rb
@@ -0,0 +1,12 @@
+class ProducerInformationSerializer < ActiveModel::Serializer
+  attributes :id, :name, :comment, image_url
+
+  # rails_blob_urlを使って画像のURLを取得
+  def image_url
+    if object.image.attached?
+      Rails.application.routes.url_helpers.rails_blob_url(object.image,
+                                                          host: ENV.fetch('RAILS_API_HOST',
+                                                                          'http://localhost:8000'))
+    end
+  end
+end
