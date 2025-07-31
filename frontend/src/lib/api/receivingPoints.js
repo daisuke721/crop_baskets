e4aed267305d6b0a7c28bfa8c74ae835ba6c77ee
daisuke721 <d.magome.07@gmail.com>
2025-07-31 16:53:59 +0900

    change: token引数を削除 #97

--- frontend/src/lib/api/receivingPoints.js
+++ frontend/src/lib/api/receivingPoints.js
@@ -13,7 +13,7 @@ export const fetchReceivingPointById = async (id) => {
 };
 
 // producerが受け取り所を新規作成するAPI
-export const createReceivingPoint = async ({ name, address, latitude, longitude }, token) => {
+export const createReceivingPoint = async ({ name, address, latitude, longitude }) => {
   const response = await apiClient.post(
     '/receiving_points',
     {
@@ -24,33 +24,22 @@ export const createReceivingPoint = async ({ name, address, latitude, longitude 
         longitude,
       },
     },
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    },
+    {},
   );
 
   return response.data;
 };
 
 // producerが受け取り所を削除するAPI
-export const deleteReceivingPoint = async (id, token) => {
-  const response = await apiClient.delete(`/receiving_points/${id}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+export const deleteReceivingPoint = async (id) => {
+  const response = await apiClient.delete(`/receiving_points/${id}`);
 
   return response.data;
 };
 
 // producerが登録した受け取り所を取得
-export const fetchMyReceivingPoints = async (token) => {
-  const response = await apiClient.get('/receiving_points/my_list', {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+export const fetchMyReceivingPoints = async () => {
+  const response = await apiClient.get('/receiving_points/my_list');
+
   return response.data;
 };
