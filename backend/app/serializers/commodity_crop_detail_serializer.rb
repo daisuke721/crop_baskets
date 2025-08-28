e27dc6be0c1e83fedac9a7405872303d65ab335d
daisuke721 <d.magome.07@gmail.com>
2025-08-28 17:15:44 +0900

    change: 生産者情報を取得 #116

--- backend/app/serializers/commodity_crop_detail_serializer.rb
+++ backend/app/serializers/commodity_crop_detail_serializer.rb
@@ -1,4 +1,5 @@
 class CommodityCropDetailSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
   # 作物の詳細に必要な基本情報
   attributes :id, :name, :variety, :harvest_day, :capacity, :price, :description, :crop_name, :crop_producing_area,
              :receiving_point, :grade, :condition
@@ -28,5 +29,16 @@ class CommodityCropDetailSerializer < ActiveModel::Serializer
       name: object.receiving_point.name,
       address: object.receiving_point.address
     }
+  end
+
+  # 生産者の公開プロフィール
+  def producer_display_name
+    object.producer&.producer_information&.name
+  end
+
+  def producer_icon_url
+    info = object.producer&.producer_information
+    return unless info&.image&.attached?
+    rails_blob_url(info.image, host: ENV.fetch('RAILS_API_HOST', 'http://localhost:8000'))
   end
 end
